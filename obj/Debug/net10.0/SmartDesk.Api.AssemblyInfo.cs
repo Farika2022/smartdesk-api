ff27ad0ca13a9c39e57dab190619c01ecb9274ad
Fariha Mahjabeen <fariha@Farihas-MacBook-Air.local>
2026-08-02 08:52:13 +0200

    add Dockerfile

--- obj/Debug/net10.0/SmartDesk.Api.AssemblyInfo.cs
+++ obj/Debug/net10.0/SmartDesk.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartDesk.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bcfa081a6dcef5ab969b30bee51165c0b0b198a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7981a517dad2a361c4708ce09b72a3ef12e1ae3")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartDesk.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartDesk.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
